c87b6ce9cb5235d84ff97f260a297fbf2a6ca162
Pavel Odvody <podvody@redhat.com>
2017-08-04 15:52:55 +0200

    Add literal value enforcement and raise func. refactor
    The library now enforces the argument to format() to be a string
    literal value to prevent misuse by passing potentiall malicious
    user-controlled value to format() in order to traverse objects
    or dump sensitive information stored in local variables.

--- format.c
+++ format.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 
 #define ERR_BUF_SIZE 512
+#define LOAD_CONST 100
 
-static void *_raise(PyObject *f, char *spec, PyObject *exc_type) {
+static void *_raise_typed_error(PyObject *f, char *spec, PyObject *exc_type) {
   char message[ERR_BUF_SIZE];
   char *name_str = NULL;
   PyObject *type = NULL;
@@ -23,6 +24,13 @@ static void *_raise(PyObject *f, char *spec, PyObject *exc_type) {
   return NULL;
 }
 
+static int _check_format_string_is_literal(PyFrameObject *frame) {
+  char *bytecode = PyString_AsString(frame->f_code->co_code);
+  /* check that the function argument is loaded using LOAD_COSNT 
+   * which means it is a literal value*/
+  return bytecode[frame->f_lasti - 3] == LOAD_CONST;
+}
+
 static PyObject *format(PyObject *self, PyObject *f) {
   PyThreadState *state = NULL;
   PyObject *r = NULL;
@@ -31,17 +39,21 @@ static PyObject *format(PyObject *self, PyObject *f) {
   state = PyThreadState_GET();
 
   if (!(PyUnicode_Check(f) || PyString_Check(f)))
-    return _raise(f, "Argument must be `str` or `unicode` not %s",
+    return _raise_typed_error(f, "Argument must be `str` or `unicode` not %s",
                   PyExc_TypeError);
+
+  if (!_check_format_string_is_literal(state->frame)) {
+    PyErr_SetString(PyExc_SyntaxError, "Argument to format() must be string literal");
+    return NULL;
+  }
 
   m = PyObject_GetAttrString(f, "format");
   if (!m)
-    return _raise(f, "No `format` method found on object: %s",
+    return _raise_typed_error(f, "No `format` method found on object: %s",
                   PyExc_AttributeError);
 
   if (!state->frame->f_locals)
     PyFrame_FastToLocals(state->frame);
-
   r = PyObject_Call(m, f, state->frame->f_locals);
   Py_XDECREF(m);
 
